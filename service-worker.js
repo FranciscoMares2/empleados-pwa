acfee931910691dee04847c1b2db866b8f1a608f
FranciscoMares2 <115095155+FranciscoMares2@users.noreply.github.com>
2024-12-05 20:07:58 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
 const cacheName = "employee-manager-v1";
 const assets = ["/", "/index.html", "/app.js", "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"];
 
-// Install event
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
@@ -10,7 +9,6 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// Fetch event
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
